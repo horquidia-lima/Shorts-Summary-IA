4f77e36d37b31231854b85d1410df628231a8d6c
Horquidia <horquidia.a.lima@gmail.com>
2024-02-15 23:10:54 -0300

    prueba

---
+++ server/download.js
@@ -0,0 +1,7 @@
+import ytdl from 'ytdl-core'
+
+import fs from 'fs'
+
+export const download = () => {
+    console.log("Realizando o download...")
+}
--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 import cors from 'cors'
 import express from 'express'
 
+import { download } from './download' 
+
 const app = express()
 app.use(cors())
 
-app.get('/summary', (request, response) => {
-  response.send('Hello Word!')
+app.get("/summary/:id", (request, response) => {
+  download()
+  response.send("ID do video: " +  request.params.id)
 })
 
 app.listen(3333, () => console.log("Server is running on port 3333"))
