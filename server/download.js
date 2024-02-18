766c97ff606117cf804a8a43bf9a9118270e15ea
Horquidia <horquidia.a.lima@gmail.com>
2024-02-17 22:34:58 -0300

    Avançando em programação: é hora do back-end

--- server/download.js
+++ server/download.js
@@ -1,7 +1,24 @@
-import ytdl from 'ytdl-core'
+import ytdl from "ytdl-core"
 
 import fs from 'fs'
 
-export const download = () => {
-    console.log("Realizando o download...")
+export const download = (videoId) => {
+    const videoURL = "https://www.youtube.com/shorts/" + videoId
+    console.log("Realizando o download do video: ", videoId)
+
+    ytdl(videoURL, { quality: "lowestaudio", filter: "audioonly"}).
+    on("info", (info) => {
+        const seconds = info.formats[0].approxDurationMs / 1000
+
+        if(seconds > 60) {
+            throw new Error("A duracao desse video e maior que 60 segundos")
+        }
+        
+    }).on("end" , () => {
+        console.log("Download do video finalizado")
+    }).on("error", (error) => {
+        console.log("Nao foi possivel fazer o download do video: ", error)
+    }).pipe(fs.createWriteStream("./tmp/audio.mp4"))
 }
+
+
--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
 import cors from 'cors'
 import express from 'express'
 
-import { download } from './download' 
+import { download } from './download.js' 
 
 const app = express()
 app.use(cors())
 
 app.get("/summary/:id", (request, response) => {
-  download()
-  response.send("ID do video: " +  request.params.id)
+  download(request.params.id)
+  response.json({result: "Download realizado com sucesso"})
 })
 
 app.listen(3333, () => console.log("Server is running on port 3333"))
